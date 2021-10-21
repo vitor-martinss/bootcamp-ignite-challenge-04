5ed396fcfb467d8c7b83e15192addff173be6605
Vitor Martins <vitor.martins@fever.pt>
2021-10-21 14:37:11 +0100

    UPD installed typescript and changed name files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
+const App = (): JSX.Element => (
+	<>
+		<GlobalStyle />
+		<Router>
+			<Routes />
+		</Router>
+	</>
 );
 
 export default App;
